1fded04657343d45b57ee41127e031eade51d656
purplecow2 <purplecow2@purplecow2ui-MacBookAir.local>
2022-12-14 13:55:58 +0900

    api 사용

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:todayworkings/field_map/field_map.dart';
 import 'package:todayworkings/field_map/floor_info.dart';
 import 'package:todayworkings/field_map/worker_phone_list.dart';
 import 'package:todayworkings/field_map/working_content.dart';
+import 'package:todayworkings/notification/write_notification.dart';
 import 'package:todayworkings/services/api_service.dart';
 
 void main() {
@@ -31,45 +32,48 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        //canvasColor: Colors.transparent,
-      ),
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          //canvasColor: Colors.transparent,
+        ),
 
-      // api 연습
-      // home: Scaffold(
-      //   appBar: AppBar(),
-      //   body: Center(
-      //     child: SingleChildScrollView(
-      //       child: FutureBuilder<ApiResponseData>(
-      //           future: ApiService().termsSelect('service'),
-      //           builder: (context, snapshot) {
-      //             return snapshot.connectionState == ConnectionState.done
-      //                 ? Text(snapshot.data!.resultData['terms_contents']
-      //                     .toString())
-      //                 : const CircularProgressIndicator();
-      //           }),
-      //     ),
-      //   ),
-      // )
+        // api 연습
+        home: Scaffold(
+          appBar: AppBar(),
+          body: Center(
+            child: SingleChildScrollView(
+              child: FutureBuilder<ApiResponseData>(
+                  future: ApiService().termsSelect('service'),
+                  builder: (context, snapshot) {
+                    return snapshot.connectionState == ConnectionState.done
+                        ? Text(
+                            snapshot.data!.resultData['terms_name'].toString())
+                        : const CircularProgressIndicator();
+                  }),
+            ),
+          ),
+        )
 
-      //home: CheckListCalendarDetail(),
-      home: CheckListCalendar(),
-      //home: CheckListModify(),
-      // home: Scaffold(
-      //   resizeToAvoidBottomInset: false,
-      //   backgroundColor: Colors.white.withOpacity(0.8),
-      //   appBar: AppBar(
-      //     centerTitle: true,
-      //     title: Text(
-      //       '현장 지도',
-      //       style: TextStyle(fontWeight: FontWeight.w600, color: Colors.black),
-      //     ),
-      //     elevation: 0,
-      //     backgroundColor: Colors.white,
-      //   ),
-      //   body: Back(),
-      // ),
-    );
+        // home: Scaffold(
+        //   resizeToAvoidBottomInset: false,
+        //   backgroundColor: Colors.white.withOpacity(0.8),
+        //   appBar: AppBar(
+        //     centerTitle: true,
+        //     title: Text(
+        //       '현장 지도',
+        //       style: TextStyle(fontWeight: FontWeight.w600, color: Colors.black),
+        //     ),
+        //     elevation: 0,
+        //     backgroundColor: Colors.white,
+        //   ),
+        //   body: Back(),
+        // ),
+
+        //home: CheckListCalendarDetail(),
+        //home: CheckListCalendar(),
+        //home: CheckListModify(),
+
+        //home: WriteNotification(),
+        );
   }
 }
